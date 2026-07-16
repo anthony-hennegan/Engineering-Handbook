44c9e45394b484cc78e43102d89292b76c397bb8
CyberSurfer108 <anthony.hennegan@gmail.com>
2026-07-16 07:42:56 -0700

    Add Python error handling drill

--- drills/Python/drill.py
+++ drills/Python/drill.py
@@ -1,60 +1,26 @@
-store_name = "Anthony's General Store"
-products = [
-    {
-        "name": "nails",
-        "price": 0.89,
-        "in_stock": True
-    },
-    {
-        "name": "hammer",
-        "price": 3.99,
-        "in_stock": False
-    },
-    {
-        "name": "wood",
-        "price": 5.50,
-        "in_stock": True
-    }
-]
-
-def format_name(name):
-    return name.strip().title()
-
-def is_available(item):
-    return item['in_stock']
-
-def display_available_products(item_list):
-    print("Inventory List")
-    print("------------------")
-    for item in item_list:
-        if is_available(item):
-            print(f"{item['name']}: ${item['price']}")
-            
-def count_available_products(product_list):
-    available_product_count = 0
+package_weight_response = input("Enter package weight: ")
+delivery_distance_response = input("Enter the delivery distance(miles): ")
+try:
+    package_weight = float(package_weight_response.strip())
+    delivery_distance = int(delivery_distance_response.strip())
     
-    for product in product_list:
-        if is_available(product):
-            available_product_count += 1
-            
-    return available_product_count
+    if package_weight <= 0:
+        print("Weight must be greater than 0.")
 
-print(f"Welcome to {store_name}")
-print("")
+    elif delivery_distance <= 0:
+        print("Distance must be greater than 0.")
+        
+    else: 
+        delivery_cost = package_weight * 0.75 + delivery_distance * 0.25
+        
+        if package_weight > 50:
+            heavy_package = 15.00
+            print(f"Delivery cost: ${delivery_cost:.2f} + Heavy package fee: ${heavy_package:.2f}")
+            print(f"Total cost: ${(delivery_cost + heavy_package):.2f}")
+        else:
+            print(f"Delivery cost: ${delivery_cost}")
+except ValueError:
+    print("Please enter a valid number.")
+    
 
-user_name_response = input("What is your name? ")
-user_name = format_name(user_name_response)
-print(f"Hello {user_name}")
-print("")
 
-view_products_response = input("Would you like to view our inventory? ")
-view_products_response = view_products_response.strip().lower()
-
-if view_products_response == "yes" or view_products_response == "y":
-    display_available_products(products)
-    print("")
-    print(f"We have {count_available_products(products)} products available.")
-elif view_products_response == "no" or view_products_response == "n":
-    print("No worries. Have a nice day.")
-else:
-    print("Please type yes or no.")
